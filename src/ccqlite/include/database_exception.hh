86a7ef43e806b4806251f9af1fe5118cf28c4a0d
Szymon Welgus <szymonwelgus@gmail.com>
2018-11-26 13:29:10 +0200

    Create exception wrapper around sqlite error codes

---
+++ src/ccqlite/include/database_exception.hh
@@ -0,0 +1,50 @@
+//  TimesheetsTracker is a desktop that aids you in tracking your timesheets
+//  and seeing what work you have done.
+//
+//  Copyright(C)<2018><Szymon Welgus>
+//
+//  This program is free software : you can redistribute it and /
+//  or modify it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation
+//  , either version 3 of the License
+//  , or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful
+//  , but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+//  Contact:
+//    szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <stdexcept>
+#include <sqlite3.h>
+
+namespace ccqlite
+{
+class database_exception : std::runtime_error
+{
+public:
+    database_exception() = delete;
+    explicit database_exception(const char* errorMessage);
+    explicit database_exception(const char* errorMessage, int ret);
+    explicit database_exception(const std::string& errorMessage);
+    explicit database_exception(const std::string& errorMessage, int ret);
+    explicit database_exception(sqlite3* handle);
+    explicit database_exception(sqlite3* handle, int ret);
+    ~database_exception() = default;
+
+    int get_error_code() const noexcept;
+    int get_extendend_error_code() const noexcept;
+
+private:
+    int mErrorCode;
+    int mExtendedErrorCode;
+};
+} // namespace ccqlite
---
+++ src/ccqlite/src/database_exception.cc
@@ -0,0 +1,73 @@
+//  TimesheetsTracker is a desktop that aids you in tracking your timesheets
+//  and seeing what work you have done.
+//
+//  Copyright(C)<2018><Szymon Welgus>
+//
+//  This program is free software : you can redistribute it and /
+//  or modify it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation
+//  , either version 3 of the License
+//  , or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful
+//  , but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+//  Contact:
+//    szymonwelgus at gmail dot com
+
+#include "database_exception.hh"
+
+namespace ccqlite
+{
+database_exception::database_exception(const char* errorMessage)
+    : std::runtime_error(errorMessage), mErrorCode(-1), mExtendedErrorCode(-1)
+{
+}
+
+database_exception::database_exception(const char* errorMessage, int ret)
+    : std::runtime_error(errorMessage), mErrorCode(ret),
+      mExtendedErrorCode(-1)
+{
+}
+
+database_exception::database_exception(const std::string& errorMessage)
+    : std::runtime_error(errorMessage), mErrorCode(-1), mExtendedErrorCode(-1)
+{
+}
+
+database_exception::database_exception(const std::string& errorMessage, int ret)
+    : std::runtime_error(errorMessage), mErrorCode(ret),
+      mExtendedErrorCode(-1)
+{
+}
+
+database_exception::database_exception(sqlite3* handle)
+    : std::runtime_error(sqlite3_errmsg(handle)),
+      mErrorCode(sqlite3_errcode(handle)),
+      mExtendedErrorCode(sqlite3_extended_errcode(handle))
+{
+}
+
+database_exception::database_exception(sqlite3* handle, int ret)
+    : std::runtime_error(sqlite3_errmsg(handle)), mErrorCode(ret),
+      mExtendedErrorCode(sqlite3_extended_errcode(handle))
+{
+}
+
+int database_exception::get_error_code() const noexcept
+{
+    return mErrorCode;
+}
+
+int database_exception::get_extendend_error_code() const noexcept
+{
+    return mExtendedErrorCode;
+}
+}
+
