a5d4c8f823983f7acd001ddcdc27867267fe5966
Szymon Welgus <szymonwelgus@gmail.com>
2018-12-10 17:05:30 +0200

    Add versioning file to track versions

---
+++ src/ccqlite/include/version.hh
@@ -0,0 +1,28 @@
+//  TimesheetsTracker is a desktop that aids you in tracking your timesheets
+//  and seeing what work you have done.
+//
+//  Copyright(C) <2018> <Szymon Welgus>
+//
+//  This program is free software : you can redistribute it and /
+//  or modify it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation
+//  , either version 3 of the License
+//  , or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful
+//  , but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+constexpr auto CCQLITE_MAJOR = 0;
+constexpr auto CCQLITE_MINOR = 4;
+constexpr auto CCQLITE_PATCH = 0;
+
+constexpr auto CCQLITE_VERSION =
+    (CCQLITE_MAJOR * 1000 + CCQLITE_MINOR * 100 + CCQLITE_PATCH);
