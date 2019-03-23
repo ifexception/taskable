15d1b22800fed9ab7e141bfe1e108e3525a9f9da
Szymon Welgus <szymonwelgus@gmail.com>
2019-03-23 12:00:03 +0200

    Update formatting to be consistent

--- cmake/FindwxWidgetsVcpkg.cmake
+++ cmake/FindwxWidgetsVcpkg.cmake
@@ -1,28 +1,26 @@
-set(VCPKG_TARGET_TRIPLET "x86-windows")
+set (VCPKG_TARGET_TRIPLET "x86-windows")
 
-message(STATUS "VCPKG_TARGET_TRIPLET: ${VCPKG_TARGET_TRIPLET}")
-message(STATUS "_VCPKG_INSTALLED_DIR: ${_VCPKG_INSTALLED_DIR}")
-message(STATUS "_VCPKG_INSTALLED_DIR/VCPKG_TARGET_TRIPLET: ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}")
+message (STATUS "VCPKG_TARGET_TRIPLET: ${VCPKG_TARGET_TRIPLET}")
+message (STATUS "_VCPKG_INSTALLED_DIR: ${_VCPKG_INSTALLED_DIR}")
+message (STATUS "_VCPKG_INSTALLED_DIR/VCPKG_TARGET_TRIPLET: ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}")
 
-if("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Debug")
-    include_directories(${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
-    set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib)
-    set(wxWidgets_LIBRARIES
+if ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Debug")
+    include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+    set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib)
+    set (wxWidgets_LIBRARIES
         ${wxWidgets_LIB_DIR}/wxbase31ud.lib
         ${wxWidgets_LIB_DIR}/wxmsw31ud_core.lib
-        #${wxWidgets_LIB_DIR}/wxmsw31ud_adv.lib
         ${wxWidgets_LIB_DIR}/libpng16d.lib
         ${wxWidgets_LIB_DIR}/zlibd.lib
         comctl32
         rpcrt4)
 
-elseif("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Release")
-    include_directories(${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
-    set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
-    set(wxWidgets_LIBRARIES
+elseif ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Release")
+    include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+    set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
+    set (wxWidgets_LIBRARIES
         ${wxWidgets_LIB_DIR}/wxbase31u.lib
         ${wxWidgets_LIB_DIR}/wxmsw31u_core.lib
-        #${wxWidgets_LIB_DIR}/wxmsw31u_adv.lib
         ${wxWidgets_LIB_DIR}/libpng16.lib
         ${wxWidgets_LIB_DIR}/zlib.lib
         comctl32
