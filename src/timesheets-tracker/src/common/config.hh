5a51b195b4b78f6cf73ed89694fc127708dc0a69
Szymon Welgus <szymonwelgus@gmail.com>
2019-03-23 12:01:58 +0200

    Add config class for configuration

---
+++ src/timesheets-tracker/src/common/config.hh
@@ -0,0 +1,36 @@
+//  TimesheetsTracker is a desktop that aids you in tracking your timesheets
+//  and seeing what work you have done.
+//
+//  Copyright(C) <2018> <Szymon Welgus>
+//
+//  This program is free software : you can redistribute it and /
+//  or modify it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation
+//  , either version 3 of the License
+//  , or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful
+//  , but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <rapidxml.hpp>
+
+namespace app::cfg
+{
+struct config {
+  public:
+    static config& get_instance();
+
+    config(const config&) = delete;
+    config& operator=(const config&) = delete;
+
+  private:
+    config() = default;
+};
+} // namespace app::cfg
