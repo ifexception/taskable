5ae63cc6ba7a11439945a5effda12f4e95b5c01f
Szymon Welgus <szymonwelgus@gmail.com>
2019-03-28 20:30:38 +0200

    Initial commit of database table setup script

---
+++ db_setup.sql
@@ -0,0 +1,70 @@
+-- wx-timesheets-tracker database setup scripts --
+
+-- employers table
+CREATE TABLE employers
+(
+    employer_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    date_created_utc INTEGER NOT NULL,
+    date_modified_utc INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+    is_active INTEGER NOT NULL
+);
+
+-- projects table
+CREATE TABLE projects
+(
+    project_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    date_created_utc INTEGER NOT NULL,
+    date_modified_utc INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+    is_active INTEGER NOT NULL,
+    employer_id INTEGER NOT NULL,
+    
+    FOREIGN KEY (employer_id) REFERENCES employers(employer_id)
+);
+
+-- tasks table
+CREATE TABLE tasks
+(
+    task_id INTEGER PRIMARY KEY NOT NULL,
+    task_date TEXT NOT NULL UNIQUE,
+    date_created_utc INTEGER NOT NULL,
+    date_modified_utc INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+    is_active INTEGER NOT NULL,
+    project_id INTEGER NOT NULL,
+    
+    FOREIGN KEY (task_id) REFERENCES projects(project_id)
+);
+
+-- categories table
+CREATE TABLE categories
+(
+    category_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    project_id INTEGER NOT NULL,
+    date_created_utc INTEGER NOT NULL,
+    date_modified_utc INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+    is_active INTEGER NOT NULL,
+    
+    FOREIGN KEY project_id REFERENCES projects(project_id)
+);
+
+-- task details table
+CREATE TABLE task_details
+(
+    task_detail_id INTEGER PRIMARY KEY NOT NULL,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
+    description TEXT NOT NULL,
+    date_created_utc INTEGER NOT NULL,
+    date_modified_utc INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+    is_active INTEGER NOT NULL,
+    project_id INTEGER NOT NULL,
+    task_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    
+    FOREIGN KEY project_id REFERENCES projects(project_id),
+    FOREIGN KEY task_id REFERENCES tasks(task_id),
+    FOREIGN KEY category_id REFERENCES categories(category_id)
+);
