411d83b419b5f7e49ab633df23fe1556998b569b
Szymon Welgus <szymonwelgus@gmail.com>
2019-06-02 16:21:21 +0200

    Initial commit of taskbaricon for program

---
+++ src/frame/taskbaricon.h
@@ -0,0 +1,42 @@
+//  TimesheetsTracker is a desktop that aids you in tracking your timesheets
+//  and seeing what work you have done.
+//
+//  Copyright(C) <2018> <Szymon Welgus>
+//
+//  This program is free software : you can redistribute it and /
+//  or modify it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation
+//  , either version 3 of the License
+//  , or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful
+//  , but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <wx/taskbar.h>
+
+namespace app::frame
+{
+class TaskBarIcon : public wxTaskBarIcon
+{
+public:
+    TaskBarIcon(wxFrame* parent);
+    virtual ~TaskBarIcon();
+
+private:
+    wxDECLARE_EVENT_TABLE();
+
+    wxMenu* CreatePopUpMenu() override;
+
+    enum
+    {
+        IDC_ADD_TASK = 0,
+        IDC_RESTORE,
+        IDC_SETTINGS
+    };
+};
+}
