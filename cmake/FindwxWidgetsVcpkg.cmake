946e7a64ad6d3c769de4f1a8e6a486cad9405728
Szymon Welgus <szymonwelgus@gmail.com>
2020-03-29 19:01:01 +0200

    Add: RelWithDebInfo configuration

--- cmake/FindwxWidgetsVcpkg.cmake
+++ cmake/FindwxWidgetsVcpkg.cmake
@@ -6,6 +6,7 @@ message (STATUS "_VCPKG_INSTALLED_DIR/VCPKG_TARGET_TRIPLET: ${_VCPKG_INSTALLED_D
 message (STATUS "_VCPKG_INSTALLED_DIR/VCPKG_TARGET_TRIPLET/debug/lib/mswud: ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud")
 
 if ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Debug")
+    message (STATUS "Setting configuration type for ${CMAKE_CONFIGURATION_TYPES}")
     include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
     set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib)
     set (wxWidgets_LIBRARIES
@@ -17,6 +18,19 @@ if ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Debug")
         rpcrt4)
 
 elseif ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Release")
+    message (STATUS "Setting configuration type for ${CMAKE_CONFIGURATION_TYPES}")
+    include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+    set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
+    set (wxWidgets_LIBRARIES
+        ${wxWidgets_LIB_DIR}/wxbase31u.lib
+        ${wxWidgets_LIB_DIR}/wxmsw31u_core.lib
+        ${wxWidgets_LIB_DIR}/libpng16.lib
+        ${wxWidgets_LIB_DIR}/zlib.lib
+        comctl32
+        rpcrt4)
+
+elseif ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "RelWithDebInfo")
+    message (STATUS "Setting configuration type for ${CMAKE_CONFIGURATION_TYPES}")
     include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
     set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
     set (wxWidgets_LIBRARIES
