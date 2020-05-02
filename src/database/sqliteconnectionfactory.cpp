f67dd817e818e567fb63475d7c0377823108bca9
Szymon Welgus <szymonwelgus@gmail.com>
2020-05-02 10:25:54 +0200

    Add: SQLite connection class

---
+++ src/database/sqliteconnectionfactory.cpp
@@ -0,0 +1,37 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2020  Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//  Contact:
+//    szymonwelgus at gmail dot com
+
+#include "sqliteconnectionfactory.h"
+
+#include "sqliteconnection.h"
+
+namespace app::db
+{
+SqliteConnectionFactory::SqliteConnectionFactory(std::string connectionString)
+    : mConnectionString(connectionString)
+{
+}
+
+std::shared_ptr<IConnection> SqliteConnectionFactory::Create()
+{
+    auto connection = std::make_shared<SqliteConnection>(mConnectionString);
+    connection->Connect();
+    return std::dynamic_pointer_cast<IConnection>(connection);
+}
+} // namespace app::db
