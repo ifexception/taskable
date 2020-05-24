9136cf5e0138deebce73ca09f6908d5f91259fc5
Szymon Welgus <szymonwelgus@gmail.com>
2020-05-24 23:45:48 +0200

    Add: Class to handle creation of entities in SetupWizard

---
+++ src/wizards/entitycompositor.cpp
@@ -0,0 +1,69 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2020  Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//  Contact:
+//    szymonwelgus at gmail dot com
+
+#include "entitycompositor.h"
+
+namespace app::wizard
+{
+EntityCompositor::EntityCompositor(std::shared_ptr<spdlog::logger> logger)
+    : pLogger(logger)
+    , mEmployerId(-1)
+    , mClientId(-1)
+{
+}
+
+bool EntityCompositor::ComposeEmployerEntity(std::unique_ptr<model::EmployerModel> employer)
+{
+    data::EmployerData employerData;
+    try {
+        mEmployerId = employerData.Create(std::move(employer));
+    } catch (const sqlite::sqlite_exception& e) {
+        pLogger->error("Error occured in category EmployerData::Create() - {0:d} : {1}", e.get_code(), e.what());
+        return false;
+    }
+
+    return true;
+}
+
+bool EntityCompositor::ComposeClientEntity(std::unique_ptr<model::ClientModel> client)
+{
+    data::ClientData clientData;
+    try {
+        mClientId = clientData.Create(std::move(client));
+    } catch (const sqlite::sqlite_exception& e) {
+        pLogger->error("Error occured in category ClientData::Create() - {0:d} : {1}", e.get_code(), e.what());
+        return false;
+    }
+
+    return true;
+}
+
+bool EntityCompositor::ComposeProjectEntity(std::unique_ptr<model::ProjectModel> project)
+{
+    data::ProjectData projectData;
+    try {
+        projectData.Create(std::move(project));
+    } catch (const sqlite::sqlite_exception& e) {
+        pLogger->error("Error occured in category ProjectData::Create() - {0:d} : {1}", e.get_code(), e.what());
+        return false;
+    }
+
+    return true;
+}
+} // namespace app::wizard
