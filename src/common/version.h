c4af5ff228c587f0be1bfe0324bfa363c3469bf9
Szymon Welgus <szymonwelgus@gmail.com>
2020-07-09 21:35:03 +0200

    Bump: Version for weekly task view

--- src/common/version.h
+++ src/common/version.h
@@ -18,7 +18,7 @@
 //    szymonwelgus at gmail dot com
 
 #define TASKABLE_MAJOR 1
-#define TASKABLE_MINOR 1
+#define TASKABLE_MINOR 3
 #define TASKABLE_PATCH 0
 
 #define TASKABLE_VERSION (TASKABLE_MAJOR * 1000 + TASKABLE_MINOR * 100 + TASKABLE_PATCH)
