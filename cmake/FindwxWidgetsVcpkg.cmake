b712036d0902bb0e82b9a73445d3f0c5755c203d
Szymon Welgus <szymonwelgus@gmail.com>
2022-12-03 21:13:04 +0200

    Bump: wxWidgets library version

--- cmake/FindwxWidgetsVcpkg.cmake
+++ cmake/FindwxWidgetsVcpkg.cmake
@@ -10,8 +10,8 @@ if ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Debug")
     include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
     set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib)
     set (wxWidgets_LIBRARIES
-        ${wxWidgets_LIB_DIR}/wxbase31ud.lib
-        ${wxWidgets_LIB_DIR}/wxmsw31ud_core.lib
+        ${wxWidgets_LIB_DIR}/wxbase32ud.lib
+        ${wxWidgets_LIB_DIR}/wxmsw32ud_core.lib
         ${wxWidgets_LIB_DIR}/libpng16d.lib
         ${wxWidgets_LIB_DIR}/zlibd.lib
         comctl32
@@ -23,7 +23,7 @@ elseif ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "Release")
     set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
     set (wxWidgets_LIBRARIES
         ${wxWidgets_LIB_DIR}/wxbase31u.lib
-        ${wxWidgets_LIB_DIR}/wxmsw31u_core.lib
+        ${wxWidgets_LIB_DIR}/wxmsw32u_core.lib
         ${wxWidgets_LIB_DIR}/libpng16.lib
         ${wxWidgets_LIB_DIR}/zlib.lib
         comctl32
@@ -34,8 +34,8 @@ elseif ("${CMAKE_CONFIGURATION_TYPES}" MATCHES "RelWithDebInfo")
     include_directories (${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
     set (wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
     set (wxWidgets_LIBRARIES
-        ${wxWidgets_LIB_DIR}/wxbase31u.lib
-        ${wxWidgets_LIB_DIR}/wxmsw31u_core.lib
+        ${wxWidgets_LIB_DIR}/wxbase32u.lib
+        ${wxWidgets_LIB_DIR}/wxmsw32u_core.lib
         ${wxWidgets_LIB_DIR}/libpng16.lib
         ${wxWidgets_LIB_DIR}/zlib.lib
         comctl32
